174d229ad0234571f6ed345ccf020a97248c2597
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-07-11 23:06:36 +0530

    Navbar Updated

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ hamburger.addEventListener("click", () => {
 const mobileDropdownLinks = document.querySelectorAll('.mobile-nav .dropdown a');
 
 mobileDropdownLinks.forEach(link => {
-    link.addEventListener('click', function(e) {
+    link.addEventListener('click', function (e) {
         e.preventDefault();
         const parentLi = this.parentElement;
         // Close other open dropdowns
@@ -23,3 +23,28 @@ mobileDropdownLinks.forEach(link => {
         parentLi.classList.toggle('open');
     });
 });
+
+
+//todo: Select a Country Region  by API -
+const countrySelect = document.querySelector('#countrySelect');
+async function loadCountriesName() {
+    try {
+        const data = await fetch('countries.json');
+        if (!data.ok) {
+            throw new Error(`Your Internet connection is Poor! ${data.status}`);
+        }
+        const result = await data.json();
+        countrySelect.innerHTML = '<option value="">Select Region</option>';
+        //? Access each Countries using forEach() -
+        result.forEach((country) => {
+            const option = document.createElement('option');
+            option.value = country;
+            option.textContent = country;
+            countrySelect.appendChild(option);
+        })
+    } catch (error) {
+        console.error('Failed to load countries', error);
+        countrySelect.innerHTML = '<option value="">Select Region</option>';
+    }
+}
+loadCountriesName();
