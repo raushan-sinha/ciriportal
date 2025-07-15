8e2820dd4462e791a193ae260c39de9ea8e3d6e4
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-07-15 20:50:43 +0530

    Distributor and Retailer Pages Updated

--- distributorPage.js
+++ distributorPage.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const uploadInput = document.querySelector('#uploadImage');
     const avatar = document.querySelector('#avatarPreview');
+    const savedImage = localStorage.getItem('userImage');
+    if (savedImage) {
+        avatar.src = savedImage;
+    }
 
     uploadInput.addEventListener('change', (e) => {
         const reader = new FileReader();
         reader.onload = function () {
             avatar.src = reader.result;
+            localStorage.setItem('userImage', reader.result);
         }
         reader.readAsDataURL(e.target.files[0]);
     });
--- retailerPage.js
+++ retailerPage.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const uploadImage = document.querySelector('#uploadImage');
     const avatar = document.querySelector('#avatarPreview');
+    const savedImage = localStorage.getItem('userImage');
+    if (savedImage) {
+        avatar.src = savedImage;
+    }
 
     uploadImage.addEventListener('change', (e) => {
         const reader = new FileReader();
         reader.onload = function () {
             avatar.src = reader.result;
+            localStorage.setItem('userImage', reader.result);
         }
         reader.readAsDataURL(e.target.files[0]);
     });
