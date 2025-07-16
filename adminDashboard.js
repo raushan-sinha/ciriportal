ab7f564e4fdd2389ae17fa5220034997baa59298
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-07-16 17:41:15 +0530

    Admin Dashboard Page Updated

--- adminDashboard.js
+++ adminDashboard.js
@@ -13,3 +13,20 @@ document.querySelectorAll('.navLinks a').forEach((link, idx) => {
     });
 });
 
+
+//todo: Image -
+const avatar = document.querySelector('#avatar');
+const uploadImage = document.querySelector('#uploadImage');
+const savedImage = localStorage.getItem('adminImage');
+if (savedImage) {
+    avatar.src = savedImage;
+}
+
+uploadImage.addEventListener('change', (e) => {
+    const reader = new FileReader();
+    reader.onload = function() {
+        avatar.src = reader.result;
+        localStorage.setItem('adminImage', reader.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+})
